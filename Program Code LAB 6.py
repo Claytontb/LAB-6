30557c1af83074a0ed21afcedfc493122af4e94c
Claytontb <78582298+Claytontb@users.noreply.github.com>
2021-05-09 16:35:06 -0700

    Add files via upload

---
+++ Program Code LAB 6.py
@@ -0,0 +1,94 @@
+Program Code 
+
+
+#simplified version of code 
+import random 
+
+def main ():
+    for 1 in range (0,
+        print('--->', i)
+        spin()
+
+def spin()
+    
+
+rand_num = random.randit(1, 20)
+output = ""
+if(rand_num > 15):
+    output = "Cherries"
+  elif(rand_num > 10): 
+    output = "Orange"
+elif(rand_num > 5): 
+    output = "Plum"
+elif(rand_num > 2): 
+    output = "Bell"
+elif(rand_num > 1): 
+    output = "Melon"
+else:
+    output = "Bar"
+  
+print(output, end="")
+
+main()
+
+
+
+#first portion of code below 
+
+import random 
+
+rand_num = random.randit(1, 20)
+output = ""
+if(rand_num > 15):
+    output = "Cherries"
+  elif(rand_num > 10): 
+    output = "Orange"
+elif(rand_num > 5): 
+    output = "Orange"
+elif(rand_num > 2): 
+    output = "Orange"
+elif(rand_num > 1): 
+    output = "Orange"
+else:
+    output = "Bar"
+  
+print(output, end="")
+
+
+
+import random 
+output = ""
+if (rand_num >15):
+    output = "Cherries"
+elif(rand_num > 10): 
+    output = "Orange"
+elif(rand_num > 5): 
+    output = "Orange"
+elif(rand_num > 2): 
+    output = "Orange"
+elif(rand_num > 1): 
+    output = "Orange"
+else:
+    output - "Bar"
+
+print(output, end="")
+
+
+import random 
+
+rand_num = random.randit(1, 20)
+output = ""
+if(rand_num > 15):
+    output = "Cherries"
+  elif(rand_num > 10): 
+    output = "Orange"
+elif(rand_num > 5): 
+    output = "Orange"
+elif(rand_num > 2): 
+    output = "Orange"
+elif(rand_num > 1): 
+    output = "Orange"
+else:
+    output = "Bar"
+  
+print(output, end="")
